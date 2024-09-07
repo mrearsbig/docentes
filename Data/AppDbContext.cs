6b109266e9b89a2bfbe918a3ad42dca009e8f736
Alexander Viafara <mrearsbig@gmail.com>
2024-09-07 11:37:41 -0500

    Agrega el orm y la conexion a la base de datos

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using docentes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace docentes.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Docente> Docentes { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using docentes.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
